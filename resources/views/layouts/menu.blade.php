969e2c9fa84367cc272e8494d1b0fd94ebc28746
Milos Jevtic <73650369+jevticmilos@users.noreply.github.com>
2022-06-10 00:57:52 +0200

    Implement admin home page

--- resources/views/layouts/menu.blade.php
+++ resources/views/layouts/menu.blade.php
@@ -1,11 +1,7 @@
 <!-- need to remove -->
 <li class="nav-item">
     <a href="{{ route('home') }}" class="nav-link {{ Request::is('home') ? 'active' : '' }}">
-        <i class="nav-icon fas fa-home"></i>
-        <p>Home</p>
+        <i class="nav-icon fas fa-envelope"></i>
+        <p>Messages</p>
     </a>
-</li>
-
-<li class="{{ Request::is('admin/messages*') ? 'active' : '' }}">
-    <a href="{{ route('admin.messages.index') }}"><i class="fa fa-edit"></i><span>Messages</span></a>
 </li>
--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -1,9 +1,7 @@
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <a href="{{ route('home') }}" class="brand-link">
-        <img src="https://assets.infyom.com/logo/blue_logo_150x150.png"
-             alt="AdminLTE Logo"
-             class="brand-image img-circle elevation-3">
-        <span class="brand-text font-weight-light">{{ config('app.name') }}</span>
+        <img src="{{ asset('/images/mebit-logo.png') }}" alt="Mebit Logo" class="brand-image img-circle elevation-3">
+        <span class="brand-text font-weight-light">Mebit</span>
     </a>
 
     <div class="sidebar">
